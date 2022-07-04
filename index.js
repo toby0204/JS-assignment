eeb9dd9f4ee1f0d53cdbdd9fa4c0dc2358fd4a77
toby0204 <ekffo0204@kau.kr>
2022-07-04 11:51:30 +0900

    v1

---
+++ index.js
@@ -0,0 +1,74 @@
+
+window.onload=function(){
+
+
+    document.getElementById("add-button").addEventListener("click",add);
+    document.getElementById("schedule").setAttribute("onkeyup","enterkey()");
+}
+
+function add(){
+
+        todo=document.getElementById("schedule").value;
+
+        let now=new Date();
+        date=now.getFullYear()+'.'+(now.getMonth()+1)+'.'+now.getDate();
+
+        check=document.createElement("INPUT");
+        check.setAttribute("type","checkbox");
+
+        table=document.getElementsByClassName("table__table");
+
+        tr=document.createElement("tr");
+        tdDate=document.createElement("td");
+        tdContent=document.createElement("td");
+        tdcheck=document.createElement("td");
+
+        tdDate.innerText=date;
+        tdContent.innerText=todo;
+        tdcheck.appendChild(check);
+    
+        tr.appendChild(tdDate);
+        tr.appendChild(tdContent);
+        tr.appendChild(tdcheck);
+    
+        table[0].appendChild(tr);
+
+        check.addEventListener("click",function(){  
+             if (confirm("삭제하시겠습니까?"))
+              {
+                check.parentNode.parentNode.remove();
+              }
+
+             else{
+                check.checked=false;
+             }
+            })
+    
+    
+    };
+
+
+    function enterkey(){
+
+    
+    if (window.event.keyCode==13){
+      add();
+    }
+  }
+   
+
+   
+
+    /* add.onclick=function(){
+
+        let todo=document.getElementById('schedule').value;
+        document.write(todo);
+
+        let time=new Date()
+        date= time.getFullYear() + '.' + time.getMonth() +'.' +time.getDate();
+        document.write(date);
+
+        let x=document.createElement("INPUT")
+        x.setAttribute("type","checkbox")
+
+    }*/
