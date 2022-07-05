fab6a97c7fbe471d35470f56df8e6382e36f55e2
toby0204 <ekffo0204@kau.kr>
2022-07-05 16:42:14 +0900

    v3

--- index.js
+++ index.js
@@ -1,58 +1,46 @@
-
 window.onload=function(){
-
-
-    document.getElementById("add-button").addEventListener("click",add);
-    document.getElementById("schedule").setAttribute("onkeyup","enterkey()");
+  document.getElementById("add-button").addEventListener("click",add);
+  document.getElementById("schedule").setAttribute("onkeyup","enterkey()");
 }
 
 function add(){
+  let todo=document.getElementById("schedule").value;
+  document.getElementById("schedule").value = "";
 
-        todo=document.getElementById("schedule").value;
+  let now = new Date();
+  let date=now.getFullYear()+'.'+(now.getMonth()+1)+'.'+now.getDate();
 
-        let now=new Date();
-        date=now.getFullYear()+'.'+(now.getMonth()+1)+'.'+now.getDate();
+  let check = document.createElement("INPUT");
+  check.setAttribute("type","checkbox");
+  check.addEventListener("click",function(){  
+    if (confirm("삭제하시겠습니까?")) {
+      check.parentNode.parentNode.remove();
+    } else {
+      check.checked=false;
+    }
+  });
 
-        check=document.createElement("INPUT");
-        check.setAttribute("type","checkbox");
+  table=document.getElementsByClassName("table__table");
 
-        table=document.getElementsByClassName("table__table");
+  tr=document.createElement("tr");
+  tdDate=document.createElement("td");
+  tdContent=document.createElement("td");
+  tdcheck=document.createElement("td");
 
-        tr=document.createElement("tr");
-        tdDate=document.createElement("td");
-        tdContent=document.createElement("td");
-        tdcheck=document.createElement("td");
+  tdDate.innerText=date;
+  tdContent.innerText=todo;
+  tdcheck.appendChild(check);
 
-        tdDate.innerText=date;
-        tdContent.innerText=todo;
-        tdcheck.appendChild(check);
-    
-        tr.appendChild(tdDate);
-        tr.appendChild(tdContent);
-        tr.appendChild(tdcheck);
-    
-        table[0].appendChild(tr);
+  tr.appendChild(tdDate);
+  tr.appendChild(tdContent);
+  tr.appendChild(tdcheck);
 
-        check.addEventListener("click",function(){  
-             if (confirm("삭제하시겠습니까?"))
-              {
-                check.parentNode.parentNode.remove();
-              }
+  table[0].appendChild(tr);
+}
 
-             else{
-                check.checked=false;
-             }
-            })
-    
-    
-    };
-
-
-    function enterkey(){
-
-    
+  function enterkey(){
     if (window.event.keyCode==13){
       add();
     }
   }
-   
+ 
